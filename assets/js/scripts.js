0e5d84ede5233fca32a228b25138ac281852bba1
Fabian Rubio <fabian.rubiocs@gmail.com>
2023-04-27 13:01:20 -0400

    add cambio de bg cuando se desliza por las secciones del html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-
+// se agrega un smooth scrooll
 $(document).ready(function(){
   $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -14,9 +14,44 @@ $(document).ready(function(){
   });
 });
 
+// se muestran los tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
+// se agrega alerta de envio de correo
 $(document).ready(function() {
   $("#enviarCorreo").click(function() {
     alert("El correo fue enviado correctamente...");
   });
 });
+
+
+// seleccionar todas las secciones con la clase "bg-section"
+const sections = document.querySelectorAll('.bg-section');
+
+// seleccionar la navbar con la clase "fixed-top"
+const navbar = document.querySelector('.fixed-top');
+
+// función que se ejecutará cuando se detecte que se ha alcanzado una sección
+function handleSectionChange(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      navbar.classList.add('bg-change');
+    } else {
+      navbar.classList.remove('bg-change');
+    }
+  });
+}
+
+// crear un observer para observar las secciones
+const observer = new IntersectionObserver(handleSectionChange);
+
+// observar cada sección
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+// observar también el footer para asegurarnos de que se elimine el fondo de la navbar al llegar al final de la página
+const footer = document.querySelector('footer');
+observer.observe(footer);
